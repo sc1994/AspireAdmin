310f1e476485d123989136dbacb3d2e3a994a589
suncheng_home <sunchengsuncheng@gmail.com>
2021-02-21 10:35:22 +0800

    1

--- asp.net/source/Aspire.FreeSql.Provider/FreeSqlStartup.cs
+++ asp.net/source/Aspire.FreeSql.Provider/FreeSqlStartup.cs
@@ -1,11 +1,25 @@
+using FreeSql;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspire.FreeSql.Provider
 {
     public static class FreeSqlStartup
     {
-        public static IServiceCollection AddFreeSql(this IServiceCollection services)
+        public static IServiceCollection AddFreeSql(
+            this IServiceCollection services,
+            string connectionString,
+            DataType dataType)
         {
+            services.AddSingleton<IFreeSql>(serviceProvider => {
+                var freeSql = new FreeSqlBuilder()
+                .UseConnectionString(dataType, connectionString)
+#if DEBUG
+                .UseAutoSyncStructure(true) // 本地自动同步数据库
+#endif
+                .Build();
+                return freeSql;
+            });
+
             return services;
         }
     }
